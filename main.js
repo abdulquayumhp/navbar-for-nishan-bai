23f930a364f393ab10ef15375cc06fa1a16a94f3
quayum quayum <96136071+abdulquayumhp@users.noreply.github.com>
2021-12-24 21:04:47 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+$('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     // $('.menu-btn i').toggleClass("active");
+    // })
+
+$('.menu-btn').click(function(){
+    $('.menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active")
+})
+
+});
+
